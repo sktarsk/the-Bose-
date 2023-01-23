98621d2ee987b5153dd55e957994eebfd379c659
sktarsk <117266861+sktarsk@users.noreply.github.com>
2023-01-23 13:22:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://sktarsk: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@github.com/sktarsk/The-Bose ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
